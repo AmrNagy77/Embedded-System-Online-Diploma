37efd2a8bc6d6a3511bf80330408931d7e17907c
Amr Nagy <nagyamr59@gmail.com>
2024-07-15 19:39:59 +0300

    aa

---
+++ Assignments/C Programming/C basics/Assignment 1/ex 7/main.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Author      : Amr Nagy
+ Description : Swap Two Numbers without temp variable.
+ ============================================================================
+ */
+
+
+#include <stdio.h>
+
+int main ()
+{
+float a,b;
+   printf("Enter a value of a : ");
+    scanf("%f",&b);
+   printf("Enter a value of b : ");
+    scanf("%f",&a);
+   printf("After swapping, value of a: %f",a);
+   printf("\nAfter swapping, value of b: %f",b);
+     
+return 0;
+}
